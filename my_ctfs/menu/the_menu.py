d0ba58b0986bb8d503fd0a4c9e1162b75deb305e
noam147 <noamar1234567@gmail.com>
2025-04-06 19:54:58 +0300

    add

--- my_ctfs/menu/the_menu.py
+++ my_ctfs/menu/the_menu.py
@@ -31,4 +31,4 @@ def route_things():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)  # Main Flask app running on port 5000
+    app.run(debug=True, port=5000)  # Main Flask app running on port 5000
