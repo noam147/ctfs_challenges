28638e14a02d14b3f0240f04a26678609b336a73
noam147 <noamar1234567@gmail.com>
2025-04-07 12:05:55 +0300

    final chall 4

--- my_ctfs/chall4/app.py
+++ my_ctfs/chall4/app.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 def index():
     with open("messages.html","r") as f:
         return f.read()
-@app.route("/our_group.txt")
+@app.route("/our_group")
 def the_full_group_chat():
     with open("full_messages.html", "r") as f:
         return f.read()
-@app.route('/Alex_Sam')
+@app.route("/Alex_Sam")
+def alex_and_sam_chat():
+    return """[06/07/19, 18:25:03] Sam: Hey Alec... can we talk for a sec?<br>
+[06/07/19, 18:25:40] Alec: What's up?<br>
+[06/07/19, 18:26:05] Sam: I didn’t mean for things to go that way in the group.<br>
+[06/07/19, 18:26:42] Alec: You kinda lost it, Sam.<br>
+[06/07/19, 18:27:10] Sam: I know, I know. I was just pissed off, but I realize now I went too far.<br>
+[06/07/19, 18:27:45] Sam: Can you add me back to the group? I promise I’ll chill.<br>
+[06/07/19, 18:28:13] Alec: It’s not just up to me, man. Jamie and the others weren’t cool with what happened either.<br>
+[00/67/51, 18:07:51] Sam: Maybe I can apologize to everyone? Just give me a chance?<br>
+[06/07/19, 18:29:24] Alec: Alright… I’ll talk to them. No promises though.<br>
+[06/07/19, 18:29:37] Sam: Thanks. I appreciate it. Really."""
+@app.route('/secrets')
 def download_zip():
     zip_path = "secret2.zip"  # Adjust this path as needed
 
